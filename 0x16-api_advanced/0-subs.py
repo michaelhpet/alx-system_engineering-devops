1df30099480d242fb8970feeb622e469ffaed3e0
Michael Peter <michaelhpet@gmail.com>
2023-08-10 23:55:30 +0100

    feat: move tests files to root

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Return total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     results = response.json().get("data")
